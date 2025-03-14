009cd9c6fe187fc5ab7d74840b7c378b9647cd89
Emre ARSLAN <emrearslan3939@gmail.com>
2025-03-14 18:03:41 +0300

    Dosyadan veri okuma

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+
 def kelime_uzunlugu_al():
     while True:
         uzunluk = input("Kelime uzunluğunu seçin (3, 4, 5, 6): ")
@@ -6,3 +8,7 @@ def kelime_uzunlugu_al():
         print("Geçersiz giriş! Lütfen 3, 4, 5 veya 6 girin.")
 
 kelime_uzunlugu = kelime_uzunlugu_al()
+
+with open("data.json", 'r', encoding='utf-8') as dosya:
+    kelimeler = json.load(dosya)[str(kelime_uzunlugu)+"_letters"]
+    # Oyunun geri kalanı buraya yazılacak
